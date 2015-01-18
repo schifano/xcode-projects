7179b2a895e480a0be48f1f056bdb269367665b3
Rachel Schifano <rschifa1@gmail.com>
2015-01-18 00:35:18 -0600

    Add view and four buttons

---
+++ apress/Restructure/Restructure/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Restructure
+//
+//  Created by Rachel Schifano on 1/18/15.
+//  Copyright (c) 2015 apress. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
