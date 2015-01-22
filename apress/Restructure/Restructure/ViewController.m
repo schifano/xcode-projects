10b6b680287a475c894383f816cbf4f03bd3e2cf
Rachel Schifano <rschifa1@gmail.com>
2015-01-22 16:12:07 -0600

    Fix button 2 outlet

--- apress/Restructure/Restructure/ViewController.m
+++ apress/Restructure/Restructure/ViewController.m
@@ -25,8 +25,13 @@
     // Do any additional setup after loading the view, typically from a nib.
     
     UIApplication *app = [UIApplication sharedApplication];
-    UIInterfaceOrientation currentOrientation = app.statusBarOrientation;
-    [self doLayoutForOrientation:currentOrientation];
+//    UIInterfaceOrientation currentOrientation = app.statusBarOrientation;
+    
+//    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(doLayoutForOrientation) name:UIDeviceOrientationDidChangeNotification object:nil];
+}
+
+-(void)viewWillLayoutSubviews {
+    [self doLayoutForOrientation];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -36,12 +41,13 @@
 
 // Called automatically after device has been rotated into a new orientation
 // Called just before final rotation animations occurred
-- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation) toInterfaceOrientation duration:(NSTimeInterval)duration {
-    [self doLayoutForOrientation:toInterfaceOrientation];
-}
+//- (void)willAnimateRotationToInterfaceOrientation:(UIInterfaceOrientation) toInterfaceOrientation duration:(NSTimeInterval)duration {
+//    [self doLayoutForOrientation:toInterfaceOrientation];
+//}
 
-- (void)doLayoutForOrientation:(UIInterfaceOrientation)orientation {
-    if (UIInterfaceOrientationIsPortrait(orientation)) {
+// New method - passes control to one of the two methods, individually setup views in correct positions
+- (void)doLayoutForOrientation {
+    if (UIInterfaceOrientationIsPortrait([[UIDevice currentDevice] orientation])) {
         [self layoutPortrait];
     } else {
         [self layoutLandscape];
@@ -52,26 +58,31 @@ static const CGFloat buttonHeight = 40;
 static const CGFloat buttonWidth = 120;
 static const CGFloat spacing = 20;
 
+// New method
 - (void)layoutPortrait {
     CGRect b = self.view.bounds;
+    
+    NSLog(@"%f,%f", self.view.bounds.size.height, self.view.bounds.size.width);
+    // TEST LOG CGRect BOUNDS
     
     CGFloat contentWidth = CGRectGetWidth(b) - (2 * spacing);
     CGFloat contentHeight = CGRectGetHeight(b) - (4 * spacing) - (2 * buttonHeight);
     
     self.contentView.frame = CGRectMake(spacing, spacing, contentWidth, contentHeight);
-    
+ 
     CGFloat buttonRow1y = contentHeight + (2 * spacing);
     CGFloat buttonRow2y = buttonRow1y + buttonHeight + spacing;
     
     CGFloat buttonCol1x = spacing;
     CGFloat buttonCol2x = CGRectGetWidth(b) - buttonWidth - spacing;
-    
+//
     self.actionButton1.frame = CGRectMake(buttonCol1x, buttonRow1y, buttonWidth, buttonHeight);
     self.actionButton2.frame = CGRectMake(buttonCol2x, buttonRow1y, buttonWidth, buttonHeight);
     self.actionButton3.frame = CGRectMake(buttonCol1x, buttonRow2y, buttonWidth, buttonHeight);
     self.actionButton4.frame = CGRectMake(buttonCol2x, buttonRow2y, buttonWidth, buttonHeight);
 }
 
+// New method
 - (void)layoutLandscape {
     CGRect b = self.view.bounds;
     
@@ -83,9 +94,9 @@ static const CGFloat spacing = 20;
     CGFloat buttonX = CGRectGetWidth(b) - buttonWidth - spacing;
     CGFloat buttonRow1y = spacing;
     CGFloat buttonRow4y = CGRectGetHeight(b) - buttonHeight - spacing;
-    CGFloat buttonRow2y = buttonRow1y + floor((buttonRow4y - buttonRow1y) * 0.667);
+    CGFloat buttonRow2y = buttonRow1y + floor((buttonRow4y - buttonRow1y) * 0.333);
     CGFloat buttonRow3y = buttonRow1y + floor((buttonRow4y - buttonRow1y) * 0.667);
-    
+//
     self.actionButton1.frame = CGRectMake(buttonX, buttonRow1y, buttonWidth, buttonHeight);
     self.actionButton2.frame = CGRectMake(buttonX, buttonRow2y, buttonWidth, buttonHeight);
     self.actionButton3.frame = CGRectMake(buttonX, buttonRow3y, buttonWidth, buttonHeight);
