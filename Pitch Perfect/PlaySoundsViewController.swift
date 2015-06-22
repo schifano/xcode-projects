1634ba4a596adc135d5bc571000ee64ea657d056
Rachel Schifano <rschifa1@gmail.com>
2015-06-22 16:02:47 -0500

    Create PlaySoundsViewController

---
+++ Pitch Perfect/PlaySoundsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PlaySoundsViewController.swift
+//  Pitch Perfect
+//
+//  Created by Rachel Schifano on 6/22/15.
+//  Copyright (c) 2015 schifano. All rights reserved.
+//
+
+import UIKit
+
+class PlaySoundsViewController: UIViewController {
+
+    @IBAction func playSlowAudio(sender: UIButton) {
+        // TODO: Add code to play audio slowly
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
