3bba510bb1c748db5c34db099f6081d4553a52ad
Rachel Schifano <rschifa1@gmail.com>
2015-06-23 16:43:22 -0500

    Add fast audio and audio reset

--- Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
+++ Pitch Perfect/Pitch Perfect/PlaySoundsViewController.swift
@@ -31,11 +31,25 @@ class PlaySoundsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    // TODO create function for repeating code in slow/fast audio
     @IBAction func playSlowAudio(sender: UIButton) {
         audioPlayer.stop()
         audioPlayer.rate = 0.5
+        audioPlayer.currentTime = 0.0 // play from beginning
         audioPlayer.play()
     }
+    
+    @IBAction func playFastAudio(sender: UIButton) {
+        audioPlayer.stop()
+        audioPlayer.rate = 2.0
+        audioPlayer.currentTime = 0.0 // play from beginning
+        audioPlayer.play()
+    }
+    
+    @IBAction func stopAudio(sender: UIButton) {
+        audioPlayer.stop()
+    }
+    
     /*
     // MARK: - Navigation
 
