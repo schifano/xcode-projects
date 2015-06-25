e2b01829912d2b4c9fa8281387ac9d1716d0f396
Rachel Schifano <rschifa1@gmail.com>
2015-06-25 15:31:52 -0500

    Reuse recording name to save phone space

--- Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/RecordSoundsViewController.swift
@@ -29,10 +29,7 @@ class RecordSoundsViewController: UIViewController {
         
         // Prepare for recording audio
         let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
-        let currentDateTime = NSDate()
-        let formatter = NSDateFormatter()
-        formatter.dateFormat = "ddMMyyyy-HHmmss"
-        let recordingName = formatter.stringFromDate(currentDateTime) + ".wav"
+        let recordingName = "my_audio.wav"
         let pathArray = [dirPath, recordingName]
         let filePath = NSURL.fileURLWithPathComponents(pathArray)
         println(filePath)
