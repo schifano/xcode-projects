bdaf6a605a65ab8ba156f49750168b885f24edd3
Rachel Schifano <rschifa1@gmail.com>
2015-06-25 16:23:09 -0500

    Create RecordedAudio class

---
+++ Pitch Perfect/RecordedAudio.swift
@@ -0,0 +1,16 @@
+//
+//  RecordedAudio.swift
+//  Pitch Perfect
+//
+//  Created by Rachel Schifano on 6/25/15.
+//  Copyright (c) 2015 schifano. All rights reserved.
+//
+
+import Foundation
+
+// Model within MVC
+class RecordedAudio: NSObject {
+    // Variables to store the filepath and title of the recorded audio
+    var filePathUrl: NSURL!
+    var title: String!
+}
