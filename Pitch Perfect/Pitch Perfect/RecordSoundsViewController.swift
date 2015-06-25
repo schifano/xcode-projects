be82b0f1ba7ae0da6c4a1d375f394051b858b873
Rachel Schifano <rschifa1@gmail.com>
2015-06-25 15:27:38 -0500

    Use unique timestamp for each file recording name

--- Pitch Perfect/Pitch Perfect/RecordSoundsViewController.swift
+++ Pitch Perfect/Pitch Perfect/RecordSoundsViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RecordSoundsViewController: UIViewController {
 
+    // Declare global AVAudioRecorder variable
+    var audioRecorder:AVAudioRecorder!
+    
     @IBOutlet weak var recordingLabel: UILabel!
     @IBOutlet weak var recordButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
@@ -22,11 +26,34 @@ class RecordSoundsViewController: UIViewController {
         recordingLabel.hidden = false
 
         println("in recordAudio") // TEST
+        
+        // Prepare for recording audio
+        let dirPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0] as! String
+        let currentDateTime = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "ddMMyyyy-HHmmss"
+        let recordingName = formatter.stringFromDate(currentDateTime) + ".wav"
+        let pathArray = [dirPath, recordingName]
+        let filePath = NSURL.fileURLWithPathComponents(pathArray)
+        println(filePath)
+        
+        // Create new recording session
+        var session = AVAudioSession.sharedInstance()
+        session.setCategory(AVAudioSessionCategoryPlayAndRecord, error: nil)
+        
+        audioRecorder = AVAudioRecorder(URL: filePath, settings: nil, error: nil)
+        audioRecorder.meteringEnabled = true
+        audioRecorder.prepareToRecord()
+        audioRecorder.record()
     }
     
     
     @IBAction func stopRecordingAudio(sender: UIButton) {
         recordingLabel.hidden = true
+        
+        audioRecorder.stop()
+        var audioSession = AVAudioSession.sharedInstance()
+        audioSession.setActive(false, error: nil)
     }
     
     override func viewDidLoad() {
@@ -46,6 +73,5 @@ class RecordSoundsViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 }
 
