e424e6c839026d783dabef75c476c7e90fdcedc7
Rachel Schifano <rschifa1@gmail.com>
2015-07-29 12:53:25 -0500

    Create storyboard version of Click Counter

--- Click Counter With Storyboard/Click Counter With Storyboard/ViewController.swift
+++ Click Counter With Storyboard/Click Counter With Storyboard/ViewController.swift
@@ -10,16 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var count = 0
+    @IBOutlet var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func incrementCount() {
+        self.count++
+        self.label.text = "\(self.count)"
     }
-
-
 }
-
