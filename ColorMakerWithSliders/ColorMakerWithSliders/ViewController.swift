e5954b47bec8793f2c58f04691404e8915add5b5
Rachel Schifano <rschifa1@gmail.com>
2015-07-29 15:59:28 -0500

    Connect outlets and actions

--- ColorMakerWithSliders/ColorMakerWithSliders/ViewController.swift
+++ ColorMakerWithSliders/ColorMakerWithSliders/ViewController.swift
@@ -10,16 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // Declare variables
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    /**
+        Changes the color of the view based on the RGB values sent by the sliders.
+    */
+    @IBAction func changeColorComponent(sender: AnyObject) {
+        
+        let r: CGFloat = CGFloat(self.redSlider.value)
+        let g: CGFloat = CGFloat(self.greenSlider.value)
+        let b: CGFloat = CGFloat(self.blueSlider.value)
+        
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
