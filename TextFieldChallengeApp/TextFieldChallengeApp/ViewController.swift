05d740bc3a340ea0082715a458299c28ac583252
Rachel Schifano <rschifa1@gmail.com>
2015-08-03 22:17:40 -0500

    Add switch

--- TextFieldChallengeApp/TextFieldChallengeApp/ViewController.swift
+++ TextFieldChallengeApp/TextFieldChallengeApp/ViewController.swift
@@ -12,6 +12,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var zipCodeTextField: UITextField!
     @IBOutlet weak var cashTextField: UITextField!
+    @IBOutlet weak var switchTextField: UITextField!
+    @IBOutlet weak var textFieldSwitch: UISwitch!
     
     // Delegate Objects
     let zipCodeTextFieldDelegate = ZipCodeTextFieldDelegate()
@@ -26,4 +28,20 @@ class ViewController: UIViewController {
     }
 
     // Text field method?
+    func textFieldShouldBeginEditing(textField: UITextField) -> Bool {
+        return self.textFieldSwitch.on
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
+    }
+    
+    @IBAction func toggleTheTextEditor(sender: AnyObject) {
+        
+        if !(sender as! UISwitch).on {
+            self.switchTextField.resignFirstResponder()
+        }
+    }
 }
