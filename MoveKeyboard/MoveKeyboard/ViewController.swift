5eb0d757d48f8882c47a1e78c7063159a0f41970
Rachel Schifano <rschifa1@gmail.com>
2015-08-11 15:54:39 -0500

    Implement keyboardWillShow

---
+++ MoveKeyboard/MoveKeyboard/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  MoveKeyboard
+//
+//  Created by Rachel Schifano on 8/11/15.
+//  Copyright (c) 2015 schifano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITextFieldDelegate {
+
+    @IBOutlet weak var bearTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        bearTextField.delegate = self
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        // Subscribe
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Unsubscribe
+        self.unsubscribeToKeyboardNotifications()
+    }
+
+    // Text Field Delegate Methods
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        
+        var newText: NSString = textField.text
+        newText = newText.stringByReplacingCharactersInRange(range, withString: string)
+        
+        return true
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    // Move the view when the keyboard shows
+    func keyboardWillShow(notification: NSNotification) {
+        if bearTextField.isFirstResponder() {
+            self.view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
+    }
+    
+    // Subscribe and unsubscribe
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+    }
+    
+    func unsubscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+    }
+}
