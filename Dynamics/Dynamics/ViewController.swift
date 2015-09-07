0eb4fa696d7948074b79ad7dd1d9e3001bd97dae
Rachel Schifano <rschifa1@gmail.com>
2015-09-06 23:11:22 -0500

    Add basic gravity

--- Dynamics/Dynamics/ViewController.swift
+++ Dynamics/Dynamics/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Gravity properties
+    var animator: UIDynamicAnimator!
+    var gravity: UIGravityBehavior!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -17,5 +21,9 @@ class ViewController: UIViewController {
         let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         square.backgroundColor = UIColor.greenColor()
         view.addSubview(square)
+        
+        animator = UIDynamicAnimator(referenceView: view)
+        gravity = UIGravityBehavior(items: [square])
+        animator.addBehavior(gravity)
     }
 }
