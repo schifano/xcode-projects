1f10aafbf0778d268b3ae800e46e39ff784459c1
Rachel Schifano <rschifa1@gmail.com>
2015-10-05 11:42:15 -0500

    fix: Run code on ViewController to add subviews to view instead of window

---
+++ BackgroundCamera/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BackgroundCamera
+//
+//  Created by Rachel Schifano on 10/5/15.
+//  Copyright © 2015 schifano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BackgroundCamera/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BackgroundCamera
+//
+//  Created by Rachel Schifano on 10/5/15.
+//  Copyright © 2015 schifano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
