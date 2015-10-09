8a2ffdecbb37a1f7c90a552685405cfc911b8a71
Rachel Schifano <rschifa1@gmail.com>
2015-10-09 15:03:10 -0500

    index on master: f54b361 Account for orientation in face detection

---
+++ BackgroundCamera/BackgroundCamera/BlurText.h
@@ -0,0 +1,13 @@
+//
+//  BlurText.h
+//  BackgroundCamera
+//
+//  Created by Rachel Schifano on 10/9/15.
+//  Copyright © 2015 schifano. All rights reserved.
+//
+
+#ifndef BlurText_h
+#define BlurText_h
+
+
+#endif /* BlurText_h */
