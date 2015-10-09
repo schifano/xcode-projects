82395088cec09f6ab8cb31a033c155d3211c3038
Rachel Schifano <rschifa1@gmail.com>
2015-10-09 13:34:20 -0500

    Account for orientation in face detection

--- BackgroundCamera/ViewController.m
+++ BackgroundCamera/ViewController.m
@@ -18,6 +18,11 @@
 
 @implementation ViewController
 
+    int timer = 0;
+    int twoFaceTimer = 0;
+    int oneFaceTimer = 0;
+    int noFaceTimer = 0;
+
 - (void)setupAVCapture {
     NSError *error = nil;
     
@@ -68,22 +73,83 @@
     }
     
     NSDictionary *imageOptions = nil;
+    UIDeviceOrientation curDeviceOrientation = [[UIDevice currentDevice] orientation];
+    int exifOrientation;
     
+    /* kCGImagePropertyOrientation values
+     The intended display orientation of the image. If present, this key is a CFNumber value with the same value as defined
+     by the TIFF and EXIF specifications -- see enumeration of integer constants.
+     The value specified where the origin (0,0) of the image is located. If not present, a value of 1 is assumed.
+     
+     used when calling featuresInImage: options: The value for this key is an integer NSNumber from 1..8 as found in kCGImagePropertyOrientation.
+     If present, the detection will be done based on that orientation but the coordinates in the returned features will still be based on those of the image. */
+    
+    enum {
+        PHOTOS_EXIF_0ROW_TOP_0COL_LEFT			= 1, //   1  =  0th row is at the top, and 0th column is on the left (THE DEFAULT).
+        PHOTOS_EXIF_0ROW_TOP_0COL_RIGHT			= 2, //   2  =  0th row is at the top, and 0th column is on the right.
+        PHOTOS_EXIF_0ROW_BOTTOM_0COL_RIGHT      = 3, //   3  =  0th row is at the bottom, and 0th column is on the right.
+        PHOTOS_EXIF_0ROW_BOTTOM_0COL_LEFT       = 4, //   4  =  0th row is at the bottom, and 0th column is on the left.
+        PHOTOS_EXIF_0ROW_LEFT_0COL_TOP          = 5, //   5  =  0th row is on the left, and 0th column is the top.
+        PHOTOS_EXIF_0ROW_RIGHT_0COL_TOP         = 6, //   6  =  0th row is on the right, and 0th column is the top.
+        PHOTOS_EXIF_0ROW_RIGHT_0COL_BOTTOM      = 7, //   7  =  0th row is on the right, and 0th column is the bottom.
+        PHOTOS_EXIF_0ROW_LEFT_0COL_BOTTOM       = 8  //   8  =  0th row is on the left, and 0th column is the bottom.
+    };
+    
+    switch (curDeviceOrientation) {
+        case UIDeviceOrientationPortraitUpsideDown:  // Device oriented vertically, home button on the top
+            exifOrientation = PHOTOS_EXIF_0ROW_LEFT_0COL_BOTTOM;
+            break;
+        case UIDeviceOrientationLandscapeLeft:       // Device oriented horizontally, home button on the right
+            if (isUsingFrontFacingCamera)
+                exifOrientation = PHOTOS_EXIF_0ROW_BOTTOM_0COL_RIGHT;
+            else
+                exifOrientation = PHOTOS_EXIF_0ROW_TOP_0COL_LEFT;
+            break;
+        case UIDeviceOrientationLandscapeRight:      // Device oriented horizontally, home button on the left
+            if (isUsingFrontFacingCamera)
+                exifOrientation = PHOTOS_EXIF_0ROW_TOP_0COL_LEFT;
+            else
+                exifOrientation = PHOTOS_EXIF_0ROW_BOTTOM_0COL_RIGHT;
+            break;
+        case UIDeviceOrientationPortrait:            // Device oriented vertically, home button on the bottom
+        default:
+            exifOrientation = PHOTOS_EXIF_0ROW_RIGHT_0COL_TOP;
+            break;
+    }
+    imageOptions = [NSDictionary dictionaryWithObject:[NSNumber numberWithInt:exifOrientation] forKey:CIDetectorImageOrientation];
     NSArray *features = [faceDetector featuresInImage:ciImage options:imageOptions];
-    
-    if (features.count != 1) {
-        // Toggle blur
-        NSLog(@"BLUR ME BLUR ME BLUR");
-        NSLog(@"Faces: %lu", features.count);
+
+    if (features.count == 1 && twoFaceTimer <= 0) {
+        oneFaceTimer = 120; // 2 sec
+        // unblur if blur is not = true
         dispatch_async(dispatch_get_main_queue(), ^{
             self.view.backgroundColor = [UIColor whiteColor];
         });
-    } else {
+        NSLog(@"1 face");
+    } else if (features.count > 1) {
+        twoFaceTimer = 300; // 5 seconds at 60 frames per second
+        // blur
         dispatch_async(dispatch_get_main_queue(), ^{
             self.view.backgroundColor = [UIColor greenColor];
         });
-        NSLog(@"HELLO I'M IN CAPTUREOUTPUT"); // TEST
-        NSLog(@"%@", features); // TEST
+        NSLog(@"More than 1 face");
+    } else {
+        // No faces
+        noFaceTimer = 180; // 3 sec
+        // blur
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.view.backgroundColor = [UIColor redColor];
+        });
+        NSLog(@"0 faces");
+    }
+    if (twoFaceTimer > 0) {
+        twoFaceTimer--;
+    }
+    if (oneFaceTimer > 0) {
+        oneFaceTimer--;
+    }
+    if (noFaceTimer > 0) {
+        noFaceTimer--;
     }
 }
 
@@ -91,6 +157,7 @@
     NSArray *devices = [AVCaptureDevice devicesWithMediaType:AVMediaTypeVideo];
     for (AVCaptureDevice *device in devices) {
         if ([device position] == AVCaptureDevicePositionFront) {
+            isUsingFrontFacingCamera = YES;
             NSLog(@"device: %@", device);
             return device;
         }
