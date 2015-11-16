e41060cfa7c85cb4dbc8a39da3495437bd88e771
Rachel Schifano <rschifa1@gmail.com>
2015-11-16 16:00:46 -0600

    Add latlon method functionality

--- FlickFinder2/FlickFinder2/ViewController.swift
+++ FlickFinder2/FlickFinder2/ViewController.swift
@@ -186,6 +186,80 @@ class ViewController: UIViewController {
                 return
             }
             
+            /* GUARD: Is "pages" key in the photosDictionary? */
+            guard let totalPages = photosDictionary["pages"] as? Int else {
+                print("Cannot find key 'pages' in \(photosDictionary)")
+                return
+            }
+            
+            /* Flickr API will only return up the 4000 images (100 per page * 40 page max) */
+            /* Pick a random page! */
+            let pageLimit = min(totalPages, 40)
+            let randomPage = Int(arc4random_uniform(UInt32(pageLimit))) + 1
+            self.getImageFromFlickrBySearchWithPage(methodArguments, pageNumber: randomPage)
+        }
+        task.resume(); // resume? where was it suspended?
+    }
+            
+    func getImageFromFlickrBySearchWithPage(methodArguments: [String: AnyObject], pageNumber: Int) {
+        /* Add the page to the method's arguments */
+        var withPageDictionary = methodArguments
+        withPageDictionary["page"] = pageNumber
+        
+        let session = NSURLSession.sharedSession()
+        let urlString = BASE_URL + escapedParameters(withPageDictionary)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            /* GUARD: Was there an error? */
+            guard (error == nil) else {
+                print("There was an error with your request: \(error)")
+                return
+            }
+            
+            /* GUARD: Did we get a successful 2XX response? */
+            guard let statusCode = (response as? NSHTTPURLResponse)?.statusCode where statusCode >= 200 && statusCode <= 299 else {
+                if let response = response as? NSHTTPURLResponse {
+                    print("Your request returned an invalid response! Status code: \(response.statusCode)!")
+                } else if let response = response {
+                    print("Your request returned an invalid response! Response: \(response)!")
+                } else {
+                    print("Your request returned an invalid response!")
+                }
+                return
+            }
+            
+            /* GUARD: Was there any data returned? */
+            guard let data = data else {
+                print("No data was returned by the request!")
+                return
+            }
+            
+            /* Parse the data! */
+            let parsedResult: AnyObject!
+            do {
+                parsedResult = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments)
+            } catch {
+                parsedResult = nil
+                print("Could not parse the data as JSON: '\(data)'")
+                return
+            }
+            
+            /* GUARD: Did Flickr return an error (stat != ok)? */
+            guard let stat = parsedResult["stat"] as? String where stat == "ok" else {
+                print("Flickr API returned an error. See error code and message in \(parsedResult)")
+                return
+            }
+            
+            /* GUARD: Is the "photos" key in our result? */
+            guard let photosDictionary = parsedResult["photos"] as? NSDictionary else {
+                print("Cannot find key 'photos' in \(parsedResult)")
+                return
+            }
+            
+            
             /* 2. Determine total number of photos */
             /* GUARD: Is the "total" key in the dictionary */
             guard let totalPhotos = (photosDictionary["total"] as? NSString)?.integerValue else {
@@ -238,7 +312,7 @@ class ViewController: UIViewController {
                 })
             }
         }
-        task.resume(); // resume? where was it suspended?
+        task.resume()
     }
     
     // MARK: Escape HTML Parameters
