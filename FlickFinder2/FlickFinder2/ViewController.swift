286d51cc99237cd3a7a22736afb4bd3a1d406f04
Rachel Schifano <rschifa1@gmail.com>
2015-11-16 15:17:27 -0600

    Add latlon method functionality

--- FlickFinder2/FlickFinder2/ViewController.swift
+++ FlickFinder2/FlickFinder2/ViewController.swift
@@ -18,6 +18,13 @@ let EXTRAS = "url_m"
 let SAFE_SEARCH = "1"
 let DATA_FORMAT = "json"
 let NO_JSON_CALLBACK = "1"
+// bbox constants
+let BOUNDING_BOX_HALF_WIDTH = 1.0
+let BOUNDING_BOX_HALF_HEIGHT = 1.0
+let LAT_MIN = -90.0
+let LAT_MAX = 90.0
+let LON_MIN = -180.0
+let LON_MAX = 180.0
 
 class ViewController: UIViewController {
     // MARK: Properties
@@ -256,7 +263,30 @@ class ViewController: UIViewController {
         /* Hide keyboard after searching */
         self.dismissAnyVisibleKeyboards()
         
-        print("Will implement this function in a later step...")
+        let methodArguments = [
+            "method": METHOD_NAME,
+            "api_key": API_KEY,
+            "bbox": createBoundingBoxString(),
+            "safe_search": SAFE_SEARCH,
+            "extras": EXTRAS,
+            "format": DATA_FORMAT,
+            "nojsoncallback": NO_JSON_CALLBACK
+        ]
+        getImageFromFlickrBySearch(methodArguments)
+    }
+    
+    // MARK: Lat/Lon Manipulation
+    func createBoundingBoxString() -> String {
+        let latitude = (self.latitudeTextField.text! as NSString).doubleValue
+        let longitude = (self.longitudeTextField.text! as NSString).doubleValue
+        
+        /* Fix added to ensure box is bounded by minimum and maximums */
+        let bottom_left_lon = max(longitude - BOUNDING_BOX_HALF_WIDTH, LON_MIN)
+        let bottom_left_lat = max(latitude - BOUNDING_BOX_HALF_HEIGHT, LAT_MIN)
+        let top_right_lon = min(longitude + BOUNDING_BOX_HALF_HEIGHT, LON_MAX)
+        let top_right_lat = min(latitude + BOUNDING_BOX_HALF_HEIGHT, LAT_MAX)
+        
+        return "\(bottom_left_lon),\(bottom_left_lat),\(top_right_lon),\(top_right_lat)"
     }
 }
 
