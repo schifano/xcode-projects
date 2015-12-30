56218bba9d7f7bd1e84e23c53649a8cc145d9555
Rachel Schifano <rschifa1@gmail.com>
2015-12-29 23:33:12 -0600

    Add dummy data to map

--- MapWithPins/MapWithPins/ViewController.swift
+++ MapWithPins/MapWithPins/ViewController.swift
@@ -15,11 +15,34 @@ class ViewController: UIViewController, MKMapViewDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Populate the dummy data
+        let locations = hardCodedLocationData()
+        var annotations = [MKPointAnnotation]()
+        
+        for dictionary in locations {
+            let lat = CLLocationDegrees(dictionary["latitude"] as! Double)
+            let lon = CLLocationDegrees(dictionary["longitude"] as! Double)
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
+            
+            let firstName = dictionary["firstName"] as! String
+            let lastName = dictionary["lastName"] as! String
+            let mediaURL = dictionary["mediaURL"] as! String
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(firstName) \(lastName)"
+            annotation.subtitle = mediaURL
+            
+            // Place annotation in an array
+            annotations.append(annotation)
+        }
+        // Add completed array of annotations to the map
+        self.mapView.addAnnotations(annotations)
     }
 
-    // TODO: Add mapview methods
-    
+    // TODO: Add dummy data
     /**
         Returns the view associated with the specified annotation object.
         - parameter mapView: The map view that requested the annotation view.
@@ -55,5 +78,45 @@ class ViewController: UIViewController, MKMapViewDelegate {
                 app.openURL(NSURL(string: toOpen)!)
             }
         }
+    }
+    
+    // MARK: Sample hardcoded data. An array of dictionaries. This would be JSON data.
+    func hardCodedLocationData() -> [[String : AnyObject]] {
+        return  [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z",
+                "firstName" : "Jessica",
+                "lastName" : "Uelmen",
+                "latitude" : 28.1461248,
+                "longitude" : -82.75676799999999,
+                "mapString" : "Tarpon Springs, FL",
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
+                "objectId" : "kj18GEaWD8",
+                "uniqueKey" : 872458750,
+                "updatedAt" : "2015-03-09T22:07:09.593Z"
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z",
+                "firstName" : "Gabrielle",
+                "lastName" : "Miller-Messner",
+                "latitude" : 35.1740471,
+                "longitude" : -79.3922539,
+                "mapString" : "Southern Pines, NC",
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
+                "objectId" : "8ZEuHF5uX8",
+                "uniqueKey" : 2256298598,
+                "updatedAt" : "2015-03-11T03:23:49.582Z"
+            ], [
+                "createdAt" : "2015-03-11T02:48:18.321Z",
+                "firstName" : "Jarrod",
+                "lastName" : "Parkes",
+                "latitude" : 34.73037,
+                "longitude" : -86.58611000000001,
+                "mapString" : "Huntsville, Alabama",
+                "mediaURL" : "https://linkedin.com/in/jarrodparkes",
+                "objectId" : "CDHfAy8sdp",
+                "uniqueKey" : 996618664,
+                "updatedAt" : "2015-03-13T03:37:58.389Z"
+            ]
+        ]
     }
 }
