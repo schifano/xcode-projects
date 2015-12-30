0e5abd89aac13937c4bb9ff773d39527bb99c54c
Rachel Schifano <rschifa1@gmail.com>
2015-12-29 22:50:46 -0600

    Implement mapView:viewForAnnotation

--- MapWithPins/MapWithPins/ViewController.swift
+++ MapWithPins/MapWithPins/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // TODO: Add mapview methods
+    
+    /**
+        Returns the view associated with the specified annotation object.
+        - parameter mapView: The map view that requested the annotation view.
+        - parameter annotation: The object representing the annotation that is about to be displayed.
+        - returns: The annotation view to display for the specified annotation or nil to disaply a standard annotation view.
+    */
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        let reuseID = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseID) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            // create pin
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseID)
+            pinView!.canShowCallout = true
+            pinView!.pinTintColor = UIColor.redColor()
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        } else {
+            pinView!.annotation = annotation
+        }
+        return pinView
     }
-
+    
 
 }
-
