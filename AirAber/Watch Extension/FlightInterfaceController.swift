8c1d262ab1f39dc59021b9ea2bb672de7bd0efad
Rachel Schifano <rschifa1@gmail.com>
2016-07-03 23:55:02 -0500

    Add IBOutlet for labels

---
+++ AirAber/Watch Extension/FlightInterfaceController.swift
@@ -0,0 +1,23 @@
+//
+//  FlightInterfaceController.swift
+//  AirAber
+//
+//  Created by Rachel Schifano on 7/3/16.
+//  Copyright © 2016 Mic Pringle. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class FlightInterfaceController: WKInterfaceController {
+
+    @IBOutlet var flightLabel: WKInterfaceLabel!
+    @IBOutlet var routeLabeL: WKInterfaceLabel!
+    @IBOutlet var boardingLabel: WKInterfaceLabel!
+    @IBOutlet var boardTimeLabel: WKInterfaceLabel!
+    @IBOutlet var statusLabel: WKInterfaceLabel!
+    @IBOutlet var gateLabel: WKInterfaceLabel!
+    @IBOutlet var seatLabel: WKInterfaceLabel!
+
+}
