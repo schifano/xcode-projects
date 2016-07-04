01c6a6c9422629a46e95f1661df7a6f1daea6415
Rachel Schifano <rschifa1@gmail.com>
2016-07-03 23:50:12 -0500

    Initial commit

---
+++ AirAber/AirAber/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  AirAber
+//
+//  Created by Mic Pringle on 05/08/2015.
+//  Copyright © 2015 Mic Pringle. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  
+  var window: UIWindow?
+  
+  func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+    return true
+  }
+
+}
---
+++ KeyboardExtension/KeyboardExtension/UIInputViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardExtension
+//
+//  Created by Rachel Schifano on 5/1/16.
+//  Copyright © 2016 schifano. All rights reserved.
+//
+
+import UIKit
+
+class UIInputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
